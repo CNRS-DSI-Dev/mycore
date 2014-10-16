ec0a08713645a94f142aa012d35b98e7d259daad
sodetemplin <david.rousse@dsi.cnrs.fr>
2014-10-16 15:57:30 +0200

    Ajout fr.php

---
+++ core/l10n/fr.php
@@ -0,0 +1,10 @@
+<?php
+$TRANSLATIONS = array(
+"Internal Server Error" => "Erreur interne du serveur",
+"Bad Gateway" => "Mauvaise réponse envoyée à un serveur intermédiaire par un autre serveur",
+"Service Unavailable" => "Service temporairement indisponible ou en maintenance",
+"Gateway Time-out" => "Temps d’attente d’une réponse d’un serveur à un serveur intermédiaire écoulé",
+"Bad Request" => "La syntaxe de la requête est erronée",
+"Error"  => "Erreur"
+);
+$PLURAL_FORMS = "nplurals=2; plural=(n > 1);";
