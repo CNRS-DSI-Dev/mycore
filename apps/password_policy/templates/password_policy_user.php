27602a8544e6c10bf990d31e1246703240b20922
sodetemplin <david.rousse@dsi.cnrs.fr>
2014-10-17 13:43:35 +0200

    Mantis 34582 - enelver ex de mot de passe

---
+++ apps/password_policy/templates/password_policy_user.php
@@ -0,0 +1,18 @@
+<form id="password_policy" class="section">
+	<h2><?php p($l->t('Password Policy Enforcement')); ?></h2>
+	<p><?php p($l->t('The following password restrictions are currently in place:')); ?></p>
+	<p><?php p($l->t('All passwords are required to be at least %d characters in length and;', $_['minlength'])); ?></p>
+	<ul style="list-style: circle; margin-left: 20px;">
+		<?php if(OC_Password_Policy::getMixedCase()){ ?>
+		<li> <?php p($l->t('Must contain UPPER and lower case characters')); ?></li>
+		<?php } ?>
+		<?php if(OC_Password_Policy::getNumbers()){ ?>
+		<li> <?php p($l->t('Must contain numbers')); ?></li>
+		<?php } ?>
+
+		<?php if(OC_Password_Policy::getSpecialChars()){ ?>
+		<li> <?php p($l->t('Must contain special characters: %s', $_['specialcharlist'])); ?></li>
+		<?php }?>
+
+	</ul>
+</form>
