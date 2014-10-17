1eb018ab5b177e2c9499a69b8b0bb75cbdb5bdfe
sodetemplin <david.rousse@dsi.cnrs.fr>
2014-10-17 13:04:02 +0200

    Mantis 34581 - franciser la page de partage public

---
+++ apps/files_sharing/l10n/fr.php
@@ -0,0 +1,6 @@
+<?php
+$TRANSLATIONS = array(
+"Add to your ownCloud" => "Ajouter à votre ownCloud",
+"Add to your space" => "Ajouter à votre espace",
+);
+$PLURAL_FORMS = "nplurals=2; plural=(n > 1);";
