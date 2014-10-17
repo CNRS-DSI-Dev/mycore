f4515d67bfe8be1dcc4db25dfb7c79958dd52dbc
sodetemplin <david.rousse@dsi.cnrs.fr>
2014-10-17 13:53:44 +0200

    Traduction login alt

--- core/l10n/fr.php
+++ core/l10n/fr.php
@@ -5,7 +5,7 @@ $TRANSLATIONS = array(
 "Service Unavailable" => "Service temporairement indisponible ou en maintenance",
 "Gateway Time-out" => "Temps d’attente d’une réponse d’un serveur à un serveur intermédiaire écoulé",
 "Bad Request" => "La syntaxe de la requête est erronée",
-"Error"  => "Erreur"
+"Error"  => "Erreur",
 "Alternative Login" => "Login alternatif",
 );
 $PLURAL_FORMS = "nplurals=2; plural=(n > 1);";
