86ff1f8ee64507b89bf1367144ac41620cc88bd0
sodetemplin <david.rousse@dsi.cnrs.fr>
2014-11-06 08:36:51 +0100

    Mantis 34668 - petit oubli

--- core/lostpassword/templates/email.php
+++ core/lostpassword/templates/email.php
@@ -5,7 +5,7 @@ $tpl = new OCP\Template('password_policy', 'password_policy_user');
 echo str_replace('{link}', $_['link'], $l->t('Use the following link to reset your password: {link}'));
 ?>
 
-<?php p($l->t('The following password restrictions are currently in place:')); ?>
+<?php if(\OCP\App::isEnabled('password_policy')){ p($l->t('The following password restrictions are currently in place:')); ?>
  
 <?php p('- '.$l->t('Length of password at least: %s', OC_Password_Policy::getMinLength())); ?>
 
@@ -19,5 +19,5 @@ echo str_replace('{link}', $_['link'], $l->t('Use the following link to reset yo
 
 <?php if(OC_Password_Policy::getSpecialChars()){ 
 	p('- '.$l->t('Password must contain following special characters: %s', OC_Password_Policy::getSpecialCharsList()));
-} ?>
+}}  ?>
 
