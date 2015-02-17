7ccf9adb3e1b4e0f120f8d1378c888f6521dff44
root <david.rousse@dsi.cnrs.fr>
2015-02-17 15:22:08 +0100

    Mantis 35599 - R dans My CoRe

--- lib/l10n/fr.php
+++ lib/l10n/fr.php
@@ -1,5 +1,5 @@
 <?php
 $TRANSLATIONS = array(
-"%s shared »%s« with you" => "My Core - %s a partagé %s avec vous",
+"%s shared »%s« with you" => "My CoRe - %s a partagé %s avec vous",
 );
 $PLURAL_FORMS = "nplurals=2; plural=(n > 1);";
