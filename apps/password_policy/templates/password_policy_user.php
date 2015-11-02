8fdc901c0a28671e2c320d771f4de0f0774d588e
Victor BORDAGE-GORRY <victor.bordage-gorry@globalis-ms.com>
2015-11-02 11:43:04 +0100

    Mantis 40179 - Add list symbol to the first item of the password contraints list

--- apps/password_policy/templates/password_policy_user.php
+++ apps/password_policy/templates/password_policy_user.php
@@ -1,8 +1,9 @@
 <form id="password_policy" class="section">
 	<h2><a name="ppe"></a><?php p($l->t('Local My CoRe password Policy Enforcement')); ?></h2>
 	<p><?php p($l->t('The following password restrictions are currently in place:')); ?></p>
-	<p><?php p($l->t('All passwords are required to be at least %d characters in length and;', $_['minlength'])); ?></p>
+
 	<ul style="list-style: circle; margin-left: 20px;">
+		<li><?php p($l->t('All passwords are required to be at least %d characters in length and;', $_['minlength'])); ?></li>
 		<?php if(OC_Password_Policy::getMixedCase()){ ?>
 		<li> <?php p($l->t('Must contain UPPER and lower case characters')); ?></li>
 		<?php } ?>
