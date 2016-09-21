99830a70521b48298297d843ac1a3e7ebc44376e
Patrick Paysant <patrick.paysant@linagora.com>
2016-09-21 11:58:09 +0200

    Add missing translations

---
+++ apps/activity/l10n/fr.js
@@ -0,0 +1,8 @@
+OC.L10N.register(
+    "activity",
+    {
+        "Activity notification" : "My Core - Notification d'activité",
+        "Number of days for activity history retention:" : "Nombre de jours de conservation d'historique de l'activité : "
+    },
+    "nplurals=2; plural=(n > 1);"
+);
---
+++ apps/files_sharing/l10n/fr.js
@@ -0,0 +1,11 @@
+OC.L10N.register(
+    "files_sharing",
+    {
+        "Add to your ownCloud" : "Ajouter à votre ownCloud",
+        "Add to your space" : "Ajouter à votre espace",
+        "Remote share" : "Partage distant",
+        "Do you want to add the remote share {name} from {owner}@{remote}?" : "Voulez vous ajouter le fichier {name} depuis {owner}@{remote} ?",
+        "Your owncloud URL" : "URL de votre owncloud"
+    },
+    "nplurals=2; plural=(n > 1);"
+);
